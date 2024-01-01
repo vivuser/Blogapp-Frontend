4b442028d1743d678e14441560d2ca7c1ccaf668
vivuser <vivekchamyal41@gmail.com>
2024-01-01 23:01:38 +0530

    redux store

---
+++ src/app/redux/authSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const storedUserData = JSON.parse(localStorage.getItem('userData'));
+
+const initialState = {
+    isAuthenticated: storedUserData !== null,
+    userData: storedUserData || null,
+};
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            localStorage.setItem('userData', JSON.stringify(action.payload));
+            state.isAuthenticated = true;
+            state.userData = action.payload;
+        },
+        logout: (state) => {
+            localStorage.removeItem('userData');
+            state.isAuthenticated = false;
+            state.userData = null;
+        },
+    },
+});
+
+export const  { login, logout } = authSlice.actions;
+export default authSlice.reducer;
