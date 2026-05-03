491f5d480bfde0c71ef5fff70e8fc1fe69366238
vivuser <vivekchamyal41@gmail.com>
2026-05-03 20:10:30 +0530

    upss

--- src/app/redux/providers.js
+++ src/app/redux/providers.js
@@ -3,17 +3,10 @@
 import { Provider } from "react-redux";
 import { useEffect } from "react";
 import store from "./store";
-import { initTracker } from "@/blocker-tracker/tracker.mjs";
+import "../tracker-init";
+
 
 export function Providers({ children }) {
-
-  useEffect(() => {
-    initTracker({
-      endpoint: "http://localhost:3001/sdk/blockers",
-      apiKey: "proj_d971ef99d15153d1",
-      environment: "development"
-    });
-  }, []);
 
   return <Provider store={store}>{children}</Provider>;
 }
